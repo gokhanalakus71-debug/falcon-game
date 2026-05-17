7382523f10645a4791f297710427f29a96ee28b3
Gokhan Alakus <gokhan.alakus71@email.com>
2026-05-17 17:20:13 +0200

    ddf

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.post("/api/image", async (req, res) => {
   const { prompt } = req.body;
