6a82586145b5df3c685453bb4072cc57175fe4b5
Gokhan Alakus <gokhan.alakus71@email.com>
2026-05-21 13:06:21 +0200

    G139

---
+++ scripts/ecs/ecsHelper.js
@@ -0,0 +1,11 @@
+function getCondition(entity) {
+  return getComponent(entity, "condition");
+}
+
+function getStats(entity) {
+  return getComponent(entity, "stats");
+}
+
+function getTraits(entity) {
+  return getComponent(entity, "traits");
+}
